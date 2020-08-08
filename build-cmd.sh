a93ecd3fba7e20455008b4b67a0e48e6e4dba98f
Rye Mutt <rye@alchemyviewer.org>
2020-08-08 04:06:33 -0400

    Fix up linux build

--- build-cmd.sh
+++ build-cmd.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TOP="$(dirname "$0")"
+cd "$(dirname "$0")"
 
 # turn on verbose debugging output for parabuild logs.
 exec 4>&1; export BASH_XTRACEFD=4; set -x
@@ -9,14 +9,7 @@ set -e
 # complain about unset env variables
 set -u
 
-PROJECT="libndofdev"
-# 2nd line of CHANGELOG is most recent version number:
-#         * 0.3 
-# Tease out just the version number from that line.
-VERSION="$(expr "$(sed -n 2p "$TOP/$PROJECT/CHANGELOG")" : ".* \([0-9]*\.[0-9]*\) *$")"
-SOURCE_DIR="$PROJECT"
-
-if [ -z "$AUTOBUILD" ] ; then 
+if [ -z "$AUTOBUILD" ] ; then
     exit 1
 fi
 
@@ -26,7 +19,23 @@ else
     autobuild="$AUTOBUILD"
 fi
 
-stage="$(pwd)"
+
+top="$(pwd)"
+stage="$(pwd)/stage"
+stage_include="$stage/include/"
+stage_debug="$stage/lib/debug/"
+stage_release="$stage/lib/release/"
+
+mkdir -p ${stage_include}
+mkdir -p ${stage_debug}
+mkdir -p ${stage_release}
+
+PROJECT="libndofdev"
+# 2nd line of CHANGELOG is most recent version number:
+#         * 0.3
+# Tease out just the version number from that line.
+VERSION="$(expr "$(sed -n 2p "$PROJECT/CHANGELOG")" : ".* \([0-9]*\.[0-9]*\) *$")"
+SOURCE_DIR="$PROJECT"
 
 "$autobuild" source_environment > "$stage/variables_setup.sh" || exit 1
 . "$stage/variables_setup.sh"
@@ -35,6 +44,7 @@ stage="$(pwd)"
 build=${AUTOBUILD_BUILD_ID:=0}
 echo "${VERSION}.${build}" > "${stage}/VERSION.txt"
 
+pushd "$SOURCE_DIR"
 case "$AUTOBUILD_PLATFORM" in
     windows*|darwin*)
         # Given forking and future development work, it seems unwise to
@@ -42,14 +52,47 @@ case "$AUTOBUILD_PLATFORM" in
         # repository in this message. Try to determine the URL of this
         # open-libndofdev repository and remove "open-" as a suggestion.
         echo "Windows/Mac libndofdev is in a separate bitbucket repository \
--- try $(hg paths default | sed -E 's/open-(libndofdev)/\1/')" 1>&2 ; exit 1
+        -- try $(hg paths default | sed -E 's/open-(libndofdev)/\1/')" 1>&2 ; exit 1
     ;;
     linux*)
-        opts="-DTARGET_OS_LINUX -m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE"
-        cmake ../libndofdev -DCMAKE_CXX_FLAGS="$opts" -DCMAKE_C_FLAGS="$opts" \
-            -DCMAKE_OSX_ARCHITECTURES="$AUTOBUILD_CONFIGURE_ARCH" \
-            -DWORD_SIZE:STRING=$AUTOBUILD_ADDRSIZE \
-            -DCMAKE_BUILD_TYPE:STRING=Release
-        make
+        opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE}"
+        DEBUG_COMMON_FLAGS="$opts -Og -g -fPIC -DPIC"
+        RELEASE_COMMON_FLAGS="$opts -O3 -g -fPIC -fstack-protector-strong -DPIC -D_FORTIFY_SOURCE=2"
+        DEBUG_CFLAGS="$DEBUG_COMMON_FLAGS"
+        RELEASE_CFLAGS="$RELEASE_COMMON_FLAGS"
+        DEBUG_CXXFLAGS="$DEBUG_COMMON_FLAGS -std=c++17"
+        RELEASE_CXXFLAGS="$RELEASE_COMMON_FLAGS -std=c++17"
+        DEBUG_CPPFLAGS="-DPIC"
+        RELEASE_CPPFLAGS="-DPIC -D_FORTIFY_SOURCE=2"
+        
+        JOBS=`cat /proc/cpuinfo | grep processor | wc -l`
+        
+        # debug build
+        CFLAGS="$DEBUG_CFLAGS -I${stage}/packages/include -Wl,-L${stage}/packages/lib/debug" \
+        CXXFLAGS="$DEBUG_CXXFLAGS -I${stage}/packages/include -Wl,-L${stage}/packages/lib/debug" \
+        CPPFLAGS="$DEBUG_CPPFLAGS -I${stage}/packages/include -Wl,-L${stage}/packages/lib/debug" \
+        LDFLAGS="$opts -L${stage}/packages/lib/debug" \
+        make all
+
+        cp libndofdev.a ${stage_debug}
+
+        make clean
+
+        # release build
+        CFLAGS="$RELEASE_CFLAGS -I${stage}/packages/include -Wl,-L${stage}/packages/lib/release" \
+        CXXFLAGS="$RELEASE_CXXFLAGS -I${stage}/packages/include -Wl,-L${stage}/packages/lib/release" \
+        CPPFLAGS="$RELEASE_CPPFLAGS -I${stage}/packages/include -Wl,-L${stage}/packages/lib/release" \
+        LDFLAGS="$opts -L${stage}/packages/lib/release" \
+        make all
+
+        cp libndofdev.a ${stage_release}
+
+        make clean
+
+        cp ndofdev_external.h ${stage_include}
     ;;
 esac
+popd
+
+mkdir -p ${stage}/LICENSES
+cp LICENSE ${stage}/LICENSES/libndofdev.txt
